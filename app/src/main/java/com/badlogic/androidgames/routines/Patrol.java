d1bae3a8cb76c4cc89ef5c2b88f261bf887b0db5
GorillaGorilla <brotherington@gmail.com>
2015-09-05 14:08:33 +0100

    Initial commit
    Initial commit to (seemingly) most up to date version of Hover boats
    when I got my laptop charger back.

---
+++ app/src/main/java/com/badlogic/androidgames/routines/Patrol.java
@@ -0,0 +1,106 @@
+package com.badlogic.androidgames.routines;
+
+import com.badlogic.androidgames.hoverboats.Ship;
+import com.badlogic.androidgames.hoverboats.World;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import framework.classes.Vector2;
+
+/**
+ * Created by New PC 2012 on 08/02/2015.
+ */
+public class Patrol extends Routine {
+
+    boolean first = true;
+    List<Vector2> points;
+    List<Routine> routines;
+    Routine sequence;
+    Routine repeat;
+
+
+    public Patrol(List<Vector2> points){
+
+        this.points = points;
+        this.routines = new ArrayList<Routine>();
+
+
+    }
+
+    @Override
+    public void reset() {
+        start();
+        first = true;
+
+    }
+
+    @Override
+    public void act(Ship ship, World world, float delta) {
+        if (isRunning()){
+            if (first){
+                findStartPoint(ship.position);
+                first = false;
+                for (Vector2 point : points){
+                    routines.add(Routines.moveTo(point.x,point.y));
+                    System.out.println("MoveTo: " + point.display());
+                }
+                sequence = Routines.sequence(routines);
+                repeat = Routines.repeat(sequence);
+                repeat.reset();
+            }
+
+            repeat.act(ship, world, delta);
+
+            if (sequence.isFailure()){
+                fail();
+                System.out.println("patrol failed");
+            }
+
+
+        }
+
+
+    }
+
+    private void findStartPoint(final Vector2 position){
+
+        Collections.sort(points,new Comparator<Vector2>(){
+            @Override public int compare(Vector2 p1, Vector2 p2){
+                return (int)(p1.dist(position) - (int)p2.dist(position));
+            }
+
+        });
+
+//
+//        float closest = 1000000f;
+//        List<Vector2> newPoints;
+//        int i = 0;
+//        int numberOfPoints = points.size();
+//        int index = 0;
+//        for (Vector2 point : points){
+//
+//            float dist = point.dist(position);
+//            if (closest > dist){
+//                closest = dist;
+//                index = i;
+//            }
+//            i ++;
+//        }
+//        if (index != 0) {
+//            System.out.println("Closest: " + index);
+//            newPoints = new ArrayList<Vector2>(numberOfPoints);
+////            for (i=0;i<numberOfPoints;i++){
+////                newPoints.add(new Vector2(0,0));
+////            }
+//            Collections.copy(newPoints,points);
+////            List<Vector2> newPoints = new List<Vector2>(points.subList(index, numberOfPoints));
+////            Collections.copy(newPoints,points.subList(index, numberOfPoints));
+////            Collections.copy(newPoints,points.subList(0, index-1));
+//            newPoints.addAll(points.subList(0, index - 1));  //Bug found. doesnt seem to like shuffling the List<>. Tried to get around using Collections.copy. No luck yet. 05/07/15
+//            points = newPoints;
+//        }
+    }
+}
---
+++ app/src/main/java/com/badlogic/androidgames/routines/SailAwaySimple.java
@@ -0,0 +1,78 @@
+package com.badlogic.androidgames.routines;
+
+import com.badlogic.androidgames.hoverboats.SailAwayFrom;
+import com.badlogic.androidgames.hoverboats.Ship;
+import com.badlogic.androidgames.hoverboats.World;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import framework.classes.Vector2;
+
+/**
+ * Created by f mgregor on 25/04/2015.
+ */
+public class SailAwaySimple extends Routine {
+    Ship self;
+    Ship enemy;
+    Routine sailAway;
+    List<Vector2> patrolPoints = new ArrayList<Vector2>();
+    boolean fleeing = false;
+
+    Routine patrol;
+
+    public SailAwaySimple(){
+        sailAway = new SailAwayFrom();
+        patrolPoints.add(new Vector2(2000f,3500f));
+        patrolPoints.add(new Vector2(2000f,3000f));
+        patrolPoints.add(new Vector2(1800f,3800f));
+        patrol = Routines.patrol(patrolPoints);
+
+
+    }
+
+    public SailAwaySimple(List<Vector2> points){
+        sailAway = new SailAwayFrom();
+        patrol = Routines.patrol(points);
+    }
+
+    @Override
+    public void reset() {
+        start();
+        sailAway.reset();
+        patrol.reset();
+    }
+
+    @Override
+    public void act(Ship ship, World world, float delta) {
+        self = ship;
+        if (!self.bb.targets.isEmpty()) {
+            enemy = ship.bb.targets.get(0);
+            if (self.position.dist(enemy.position)<200 && !fleeing){
+                fleeing = true;
+                patrol.reset();
+                sailAway.act(ship,world,delta);
+            }
+            else if (fleeing){
+                sailAway.act(ship,world,delta);
+                if (self.position.dist(enemy.position)>800){
+                    fleeing = false;
+                    sailAway.reset();
+                }
+            }else{
+                patrol.act(self,world,delta);
+            }
+
+
+        }
+        else {
+//            only happens if boat has no targets/enemies
+            patrol.act(self,world,delta);
+            fleeing = false;
+        }
+
+        if (patrol.isFailure()){
+            fail();
+        }
+    }
+}
